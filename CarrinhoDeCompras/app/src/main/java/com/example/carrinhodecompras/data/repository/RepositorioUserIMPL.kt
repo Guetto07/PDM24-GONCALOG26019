1d2f2ef1729abcbc6ad8a829f0181854d56bc6c8
Guetto07 <a26019@alunos.ipca.pt>
2025-01-05 18:09:31 +0000

    small fix pt2

--- CarrinhoDeCompras/app/src/main/java/com/example/carrinhodecompras/data/repository/RepositorioUserIMPL.kt
+++ CarrinhoDeCompras/app/src/main/java/com/example/carrinhodecompras/data/repository/RepositorioUserIMPL.kt
@@ -4,8 +4,8 @@ import com.example.carrinhodecompras.domain.model.User
 import com.example.carrinhodecompras.domain.repository.RepositorioUser
 
 class RepositorioUserIMPL : RepositorioUser {
-    override suspend fun fazerLogin(email: String, senha: String): User? {
-        
+    override suspend fun Login(email: String, senha: String): User? {
+
         return if (email == "usuario@example.com" && senha == "senha123") {
             User(id = "1", email = email, senha = senha)
         } else {
