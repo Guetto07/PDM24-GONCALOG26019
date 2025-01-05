fa4e401d62d5634b303de3b77925fa91f5cad617
Guetto07 <a26019@alunos.ipca.pt>
2025-01-05 19:17:08 +0000

    small fix

---
+++ CarrinhoDeCompras/app/src/main/java/com/example/carrinhodecompras/presentation/screens/LoginScreen.kt
@@ -0,0 +1,9 @@
+package com.example.carrinhodecompras.presentation.screens
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun LoginScreen(navController: NavController) {
+    // Implementação da UI de Login
+}
