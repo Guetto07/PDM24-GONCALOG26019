cf698d5cf17fe45fae15a29b3fb1b396581e9d75
Guetto07 <a26019@alunos.ipca.pt>
2025-01-06 18:54:14 +0000

    fix

--- CarrinhoDeCompras/app/src/main/java/com/example/carrinhodecompras/MainActivity.kt
+++ CarrinhoDeCompras/app/src/main/java/com/example/carrinhodecompras/MainActivity.kt
@@ -6,7 +6,6 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.navigation.compose.rememberNavController
 import com.example.carrinhodecompras.presentation.navigation.NavigationGraph
-import com.example.carrinhodecompras.ui.theme.CarrinhoDeComprasTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
